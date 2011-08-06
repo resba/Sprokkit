c94c70762fae0ff7ced7d4aff9e66f1691b1beeb
resba <thatwebguymatt@gmail.com>
2011-08-06 03:00:57 -0400

    Removing Debug Relays for Production Testing.

--- sprokkit.py
+++ sprokkit.py
@@ -40,35 +40,35 @@ while 1:
 
     def filterResponse():
         sentmessage = data
-        woot.send ( 'PRIVMSG '+channel+' :Loaded filterResponse Function with '+sentmessage+' as the trigger. \r\n' )
+#        woot.send ( 'PRIVMSG '+channel+' :Loaded filterResponse Function with '+sentmessage+' as the trigger. \r\n' )
         #The command has been called. First check to see what type of command was called.
         if data.find ( ':!' ) != -1:
             global messageable 
             messageable = channel
-            woot.send ( 'PRIVMSG '+channel+' :The command was an announement ! \r\n' )
+#            woot.send ( 'PRIVMSG '+channel+' :The command was an announement ! \r\n' )
             #The command was an announcement. now we check for privilages.
             mySubString = sentmessage[sentmessage.find(":")+1:sentmessage.find("!")]
-            woot.send ( 'PRIVMSG '+channel+' :Last Message: %s\r\n'%mySubString )
+#            woot.send ( 'PRIVMSG '+channel+' :Last Message: %s\r\n'%mySubString )
             atsymbol = "@"
             voicesymbol = "+"
             #If the nameslist variable contains the user with some sort of privilage. The check ends and returns to the command.
             if nameslist.find(atsymbol+mySubString) != -1:           
-                woot.send ( 'PRIVMSG '+channel+' :You are an op \r\n' )
+#                woot.send ( 'PRIVMSG '+channel+' :You are an op \r\n' )
                 #because this is a global filter, the messageable is named the channel because its an announcement.
                 return 0
             elif nameslist.find(voicesymbol+mySubString) != -1:
-            	woot.send ( 'PRIVMSG '+channel+' :You are voiced \r\n' )
+#            	woot.send ( 'PRIVMSG '+channel+' :You are voiced \r\n' )
                 return 0
             else:
                 #If the user is NOT privilidged, then they need to jump through a few more hoops.
-                woot.send ( 'PRIVMSG '+channel+' :You are not a privilidged user \r\n' )
+#                woot.send ( 'PRIVMSG '+channel+' :You are not a privilidged user \r\n' )
                 if(time.time() - lastUsed) > 10:
                     global lastUsed
                     lastUsed = time.time()
-                    woot.send ('PRIVMSG '+channel+' :lastUsed Check Passed, now returning to command \r\n' )
+#                    woot.send ('PRIVMSG '+channel+' :lastUsed Check Passed, now returning to command \r\n' )
                     return 0
                 else:
-                    woot.send ( 'PRIVMSG '+channel+' :Command Cooldown Active. Ignoring Command \r\n' )
+#                    woot.send ( 'PRIVMSG '+channel+' :Command Cooldown Active. Ignoring Command \r\n' )
                     return 1
         elif data.find ( ':^' ) != -1:
             #The Command was a Privmsg, so we send the privmsg.
@@ -82,17 +82,17 @@ while 1:
         woot.send( 'JOIN '+channel+'\r\n' )
     if data.find ( '353' ) != -1:
         nameslist = data
-        woot.send( 'PRIVMSG '+channel+' :Found new NAMES Listing: %s\r\n' %nameslist )
+#        woot.send( 'PRIVMSG '+channel+' :Found new NAMES Listing: %s\r\n' %nameslist )
     if data.find ( 'PING' ) != -1:
         woot.send( 'PONG ' + data.split() [1] + '\r\n')        	
     if data.find ( 'MODE' ) != -1:
-        woot.send ( 'PRIVMSG '+channel+' :MODE Command Was Sent. \r\n' )
+#        woot.send ( 'PRIVMSG '+channel+' :MODE Command Was Sent. \r\n' )
         woot.send ( 'NAMES ' + channel + ' \r\n' )
 
     if data.find ( 'test' ) != -1:
         if (filterResponse() == 0):
             woot.send( 'PRIVMSG '+messageable+' :Test command \r\n' ) 
-
+"""
 # Debug commands to help with Sprokkit Development.
     if data.find ( '!debug:say' ) != -1:
         lastMessage = data
@@ -107,28 +107,5 @@ while 1:
         woot.send ("PRIVMSG "+channel+" :%s\r\n" % lastUsed )
     if data.find ( '!debug:time.time' ) != -1:
         woot.send ("PRIVMSG "+channel+" :%s\r\n" % time.time() )
-
-"""
-# Command Cooldown function. Basically checks to see if its been about 5 seconds
-# before someone is allowed to use another one of Wikkity's commands.
-    def commandCooldown():
-#        woot.send ( 'PRIVMSG '+channel+' :Loaded commandCooldown Function \r\n' )
-        if(time.time() - lastUsed) > 10:
-            global lastUsed
-            lastUsed = time.time()
-#            woot.send ("PRIVMSG "+channel+" :lastUsed Check Passed, now returning to command %s\r\n" % globalnullvalue)
-            return 0
-        else:
-#            woot.send ( 'PRIVMSG '+channel+' :Command Cooldown Active. Ignoring Command \r\n' )
-            return 1
-#        woot.send ( 'PRIVMSG '+channel+' :lastUsed Check Completed \r\n' )
-    def readUser():
-    	if data.find ( '^' ) != -1:
-    	    lastmessage = data
-            global readUserName
-            readUserName = lastmessage[lastmessage.find(":")+1:lastmessage.find("!")]
-            messageable = readUserName
-            return 0
-        elif data.find( '!' ) != -1:
-            return 0
+        
 """
